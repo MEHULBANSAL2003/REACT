03c8799f13d8c4b4cf8b64463be5a38fe47fe2df
Mehul Bansal <mehulbansal83606@gmail.com>
2024-09-26 02:58:18 +0530

    ep-3 react component

--- ep3/App.js
+++ ep3/App.js
@@ -4,20 +4,32 @@ import ReactDOM from "react-dom/client";
 
 
 
-const jsxHeading=(<h1 className="head" tabIndex="5">
-  Hello world
-  </h1>
-)
+
 // jsxheading is react element  
 
 
 // react functional component
 
+const Title=()=> {
+  return (
+   <>
+    
+  <h1 className="head" tabIndex="5">Hello world</h1>
+  
+  </>
+)
+}
 const HeadingComponent=()=>{
   return (
-  <h1>functional component</h1>
+    <div id="container">
+<Title/>    
+  <h1 className="heading">functional component</h1>
+  </div>
   ) 
 }
+
+
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
