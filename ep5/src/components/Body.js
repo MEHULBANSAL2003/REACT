43b46af7ce6ea67de6a1017f4f2dd8ebe8b6cabc
Mehul Bansal <mehulbansal83606@gmail.com>
2024-09-27 23:35:55 +0530

    ep-5 useSate() hook

--- ep5/src/components/Body.js
+++ ep5/src/components/Body.js
@@ -1,13 +1,26 @@
-import RestaurantCard from "../RestaurantCard";
+import RestaurantCard from "./RestaurantCard";
+import {useState} from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
+   
+    let [listOfRestraunts,setListOfRestaurants]=useState(resList);
+    
+
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+
+            <button className="filter-btn" onClick={()=>{
+               const newData=listOfRestraunts.filter((res)=>{
+                    return res.info.avgRating>4.5;
+               })
+               setListOfRestaurants(newData);
+            }}>Top rated Restaurants</button>
+        </div>
   
         <div className="res-container">
-          {resList.map((restaurant)=>{
+          {listOfRestraunts.map((restaurant)=>{
             return <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
           }
   
