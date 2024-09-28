bda294137d899fdc7478e89b0ff9f4a657e0dd8e
Mehul Bansal <mehulbansal83606@gmail.com>
2024-09-29 00:26:35 +0530

    useEffect and fetching data from api

--- ep5/src/components/Body.js
+++ ep5/src/components/Body.js
@@ -1,10 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
-import {useState} from "react";
-import resList from "../utils/mockData";
+import {useState,useEffect} from "react";
+import Shimmer  from "./Shimmer";
 
 const Body = () => {
    
-    let [listOfRestraunts,setListOfRestaurants]=useState(resList);
+    let [listOfRestraunts,setListOfRestaurants]=useState([]);
+
+    useEffect(()=>{
+        console.log("hello");
+        fetchData();  
+    },[]);
+    
+    const fetchData=async()=>{
+      const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.76648186047635&lng=76.78531226911772&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const json=await data.json();
+
+      console.log(json);
+      setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    if(listOfRestraunts.length===0){
+      return <Shimmer/>
+    }
     
 
     return (
