675ddd226b552883a54ce355b00e99a1e632feb9
Mehul Bansal <mehulbansal83606@gmail.com>
2024-09-30 22:07:17 +0530

    handling error using useRouterError() hook

--- ep7/src/components/Error.js
+++ ep7/src/components/Error.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useRouteError } from 'react-router-dom';
 
 const Error = () => {
+    const error=useRouteError();
   return (
     <div>
       <h1>Oops!! Something Went Wrong</h1>
+      <h2>{error.status}: {error.statusText}</h2>
     </div>
   )
 }
