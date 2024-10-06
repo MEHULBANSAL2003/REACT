3cce6a07f0c9404b4f0733d9c22ff08a39caf8c3
Mehul Bansal <mehulbansal83606@gmail.com>
2024-10-06 23:07:34 +0530

    fetching menu data

--- ep7/src/components/RestaurantMenu.js
+++ ep7/src/components/RestaurantMenu.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import Shimmer from './Shimmer';
+import { CDN_URL } from '../utils/constants';
 
 const RestaurantMenu = () => {
-  return (
+
+    const [resInfo,setResInfo]=useState(null);
+
+    useEffect(()=>{ 
+      fetchMenu();
+    },[]);
+
+    fetchMenu=async()=>{
+        const data =await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.76648186047635&lng=76.78531226911772&restaurantId=42244&catalog_qa=undefined&submitAction=ENTER")
+        const json=await data.json();
+        console.log(json);
+        setResInfo(json.data);
+    };
+    if(resInfo===null){
+       return <Shimmer/>
+    }
+       
+     const {name,cuisines,costForTwoMessage,cloudinaryImageId,avgRating}=resInfo?.cards[2]?.card?.card?.info;
+
+  return  (
     <div>
-      <h1>Name of the Restaurant</h1>
-      <h2>Menu</h2>
+      <h1>{name}</h1>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={CDN_URL+cloudinaryImageId}
+      />
+      <p>{cuisines.join(",")} -{costForTwoMessage}</p>
+      
+      <h2>{avgRating}</h2>
       <ul>
         <li>Burger</li>
         <li>Noodles</li>
