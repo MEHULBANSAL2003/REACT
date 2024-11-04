69d332730096ab291c2b432039be6cb76c8018bd
Mehul Bansal <mehulbansal83606@gmail.com>
2024-11-04 12:17:23 +0530

    some css

--- ep7/src/components/Body.js
+++ ep7/src/components/Body.js
@@ -7,12 +7,12 @@ const Body = () => {
   let [filteredRestaurant, setFilteredRestaurant] = useState([]);
   const [searchText, setSearchText] = useState("");
 
-  const [currStatus,setCurrStatus]=useState("Top rated restaurant");
+  const [currStatus, setCurrStatus] = useState("Top rated restaurant");
 
   useEffect(() => {
     console.log("hello");
     fetchData();
-  },[]);
+  }, []);
 
   const fetchData = async () => {
     const data = await fetch(
@@ -60,21 +60,17 @@ const Body = () => {
         <button
           className="filter-btn"
           onClick={() => {
-            if(currStatus==="Top rated restaurant"){
+            if (currStatus === "Top rated restaurant") {
               setCurrStatus("all restuarants");
-            const newData = listOfRestraunts.filter((res) => {
-              return res.info.avgRating > 4.5;
-            });
-            setFilteredRestaurant(newData);
-          }
-          else{
-            setCurrStatus("Top rated restaurant");
+              const newData = listOfRestraunts.filter((res) => {
+                return res.info.avgRating > 4.5;
+              });
+              setFilteredRestaurant(newData);
+            } else {
+              setCurrStatus("Top rated restaurant");
 
-            fetchData();
-              
-            
-          }
-         
+              fetchData();
+            }
           }}
         >
           {currStatus}
