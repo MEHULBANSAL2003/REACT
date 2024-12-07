6206f81787cf6478ffccdb18b4958c8b4056f555
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-07 23:14:41 +0530

    applying tailwind css and styling the  header

--- ep7/src/components/Header.js
+++ ep7/src/components/Header.js
@@ -8,42 +8,42 @@ import useOnline from "../utils/useOnline";
 const Header = () => {
 
     //let btnName="logout";
-    let [btnName,setbtnName]=useState("login");
+    let [btnName,setbtnName]=useState("Sign in");
 
     const isOnline=useOnline();
    
     
     return (
-      <div className="header">
+      <div className="flex justify-between bg-orange-500 shadow-lg">
         <div className="logo-container">
           <img
-            className="logo"
+            className="h-24 p-2 "
             src={LOGO_URL}
           />
         </div>
   
         <div className="nav-items">
-          <ul>
-            <li>
+          <ul className="flex py-8 ">
+            <li className="px-2 font-bold text-white">
               <Link to="/">Home</Link>
               </li>
-            <li>
+            <li className="px-2 font-bold text-white">
               <Link to="/about">About Us</Link>
               </li>
-            <li>
+            <li className="px-2 font-bold text-white">
               <Link to="/contact">Contact</Link>
               </li>
-            <li>
+            <li className="px-2 font-bold text-white">
               Cart</li>
-              <li>
+              <li className="px-2 font-bold text-white">
               <Link to="/instamart">Instamart</Link>
-              </li>
+              </li >
 
-              <li>{isOnline?"✅":"❌"}</li>
+              <li className="px-2 ">{isOnline?"✅":"❌"}</li>
 
-            <button className="login" onClick={()=>{
+            <button className="border-4 border-black rounded-md text-white bg-black px-2"nClick={()=>{
 
-              btnName==="login"?setbtnName("logout"):setbtnName("login");
+              btnName==="Sign in"?setbtnName("Logout"):setbtnName("Sign in");
             }}>{btnName}</button>
           </ul>
         </div>
