007ff7e787adbce928b2947a04f38bbf7607b84f
Mehul Bansal <mehulbansal83606@gmail.com>
2024-12-08 15:07:26 +0530

    updated restro menu format

--- ep7/src/utils/constants.js
+++ ep7/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const LOGO_URL="https://t3.ftcdn.net/jpg/04/03/74/22/360_F_403742248_8DDzcFF4jw05lWqftk2yxzKRpFvpZ01Y.jpg";
+export const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbrEoGN30ycyxwFM_KY3t_zSrkfMaQ_3e7ow&s";
 
 export const MENU_URL="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.76648186047635&lng=76.78531226911772&restaurantId="
 
